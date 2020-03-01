081c7627993dcd4f2e856a85b8a1b7650ca44b34
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2020-03-01 20:34:21 +0100

    getIgMedia and updateIgMedia implemented

--- handler.js
+++ handler.js
@@ -2,7 +2,17 @@
 const logger = require('pino')()
 const Kazzenger = require('./src/kazzenger')
 const moment = require('moment')
+const axios = require('axios')
+const log = require('pino')()
+const { Media, sequelize } = require('./media')
+const { IG_ACCESS_TOKEN, IG_CLIENT_ID } = process.env
 
+const igClient = axios.create({
+  baseURL: 'https://graph.instagram.com',
+  params: {
+    access_token: IG_ACCESS_TOKEN,
+  },
+})
 module.exports.bridges = (event, context, callback) => {
   if (event.source === 'serverless-plugin-warmup') {
     logger.info('WarmUP - Lambda is warm!')
@@ -28,4 +38,61 @@ module.exports.bridges = (event, context, callback) => {
     body: JSON.stringify(calculatedBridges),
   }
   callback(null, response)
+}
+
+module.exports.updateIgMedia = (event, context, callback) => {
+  let mediaIds = []
+  Media.sync({ force: true })
+    .then(() => {
+      igClient.get(`${IG_CLIENT_ID}/media?access_token=${IG_ACCESS_TOKEN}`)
+        .then(listResponse => {
+          const { data } = listResponse.data
+          const recentMedia = data.slice(0, 5)
+          Promise.all(recentMedia.map(media => {
+            return igClient.get(`${media.id}?access_token=${IG_ACCESS_TOKEN}&fields=caption,media_url,permalink`)
+          }))
+            .then(responses => {
+              mediaIds = responses.map(resp => resp.data.id)
+              Media.bulkCreate(responses.map(response => {
+                return {
+                  mediaId: response.data.id,
+                  mediaUrl: response.data.media_url,
+                  permaurl: response.data.permalink,
+                  caption: response.data.caption,
+                }
+              }))
+              const response = {
+                isBase64Encoded: false,
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify({
+                  mediaIds,
+                }),
+              }
+              callback(null, response)
+            })
+            .catch(allError => {
+              log.error(allError)
+            })
+        })
+        .catch(error => {
+          log.error(error)
+        })
+    })
+}
+module.exports.getIgMedia = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  let media = []
+  Media.findAll().then(async res => {
+    media = res.map(data => data.dataValues)
+    const response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify({
+        media,
+      }),
+    }
+    callback(null, response)
+  })
 }
---
+++ media.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const { MYSQL_URL } = process.env
+const sequelize = new Sequelize(MYSQL_URL, {
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 100,
+    handleDisconnects: true,
+  },
+})
+const { Model } = Sequelize
+class Media extends Model {}
+Media.init({
+  // attributes
+  mediaId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  mediaUrl: {
+    type: Sequelize.STRING,
+    // allowNull defaults to true
+  },
+  permaurl: {
+    type: Sequelize.STRING,
+  },
+  caption: {
+    type: Sequelize.STRING,
+  },
+}, {
+  sequelize,
+  modelName: 'media',
+  // options
+})
+module.exports = {
+  Media,
+  sequelize,
+}
