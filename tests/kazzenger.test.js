34cbdc76e74b9b8e0e1d90cf825d9fc5a6f89709
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2021-06-26 10:35:50 +0200

    using utc date

--- tests/kazzenger.test.js
+++ tests/kazzenger.test.js
@@ -10,20 +10,24 @@ tap.test('kazzenger function', t => {
     const kazzenger = new Kazzenger(config)
     const expectedResponse = [
       {
-        start: moment('2019-12-21T22:00:00.000Z').toDate(),
-        end: moment('2019-12-26T22:00:00.000Z').toDate(),
+        start: moment('2019-12-21T22:00:00.000Z').utc()
+          .toDate(),
+        end: moment('2019-12-26T22:00:00.000Z').utc()
+          .toDate(),
         holidaysCount: 4,
         weekdaysCount: 2,
         daysCount: 6 },
-      { start: moment('2019-12-25T22:00:00.000Z').toDate(),
-        end: moment('2019-12-29T22:00:00.000Z').toDate(),
-        holidaysCount: 4,
-        weekdaysCount: 1,
-        daysCount: 5 }]
-    const actualResponse = kazzenger.bridges({
-      start: moment('2019-09-01')
+      { start: moment('2019-12-25T22:00:00.000Z').utc()
         .toDate(),
-      end: moment('2019-12-31')
+      end: moment('2019-12-29T22:00:00.000Z').utc()
+        .toDate(),
+      holidaysCount: 4,
+      weekdaysCount: 1,
+      daysCount: 5 }]
+    const actualResponse = kazzenger.bridges({
+      start: moment('2019-09-01').utc()
+        .toDate(),
+      end: moment('2019-12-31').utc()
         .toDate(),
       maxHolidaysDistance: 2,
       maxAvailability: 2,
