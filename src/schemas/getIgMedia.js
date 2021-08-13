46965b9de72f04f673bc5a12d7d409425d154451
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2021-08-13 16:43:18 +0200

    added timestamp and changed output schema of getIgMedia

--- src/schemas/getIgMedia.js
+++ src/schemas/getIgMedia.js
@@ -3,31 +3,36 @@
 module.exports = {
   response: {
     200: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          mediaId: {
-            type: 'string',
-            description: 'If of the media',
-          },
-          mediaUrl: {
-            type: 'string',
-            description: 'Url of the image to display',
-          },
-          permaurl: {
-            type: 'string',
-            description: 'Url of the instagram post',
-          },
-          caption: {
-            type: 'string',
-            description: 'Caption of the image',
-          },
-          createdAt: {
-            type: 'string',
-          },
-          updatedAt: {
-            type: 'string',
+      type: 'object',
+      properties: {
+        media: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              mediaId: {
+                type: 'string',
+                description: 'If of the media',
+              },
+              mediaUrl: {
+                type: 'string',
+                description: 'Url of the image to display',
+              },
+              permaurl: {
+                type: 'string',
+                description: 'Url of the instagram post',
+              },
+              caption: {
+                type: 'string',
+                description: 'Caption of the image',
+              },
+              createdAt: {
+                type: 'string',
+              },
+              updatedAt: {
+                type: 'string',
+              },
+            },
           },
         },
       },
